6e70c378b64d3adb63110d7ea2d0e4d125e0858d
lzl74 <74@zjut.edu.cn>
2021-12-22 14:03:43 +0800

    Update main.py
    由于推送不太好用 更新写日志机制

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from ths_trader import THSTrader
 from spider import EastSpider
 from config import ths_xiadan_path, SCKey
 import sys
+import os
 
 
 def job():
@@ -14,18 +15,22 @@ def job():
         ths_trader = THSTrader(ths_xiadan_path)
         spider = EastSpider()
         today_kzz_list = spider.get_today_list()
+        with open(r'C:\Users\luozh\Desktop\kzz\log.txt', 'a') as f:
+            f.write('今天有'+str(len(today_kzz_list))+'只可转债'+'\n')
         if not today_kzz_list:
-            raise ValueError("今日没有可申购的可转债")
+            raise ValueError("今天没有可转债")
         for stock_id, name in today_kzz_list:
             try:
+                with open(r'C:\Users\luozh\Desktop\kzz\log.txt', 'a') as f:
+                    f.write(stock_id+' '+name+'\n')
                 push_message += str(ths_trader.buy(stock_id, 100, 10000)) + '\n'
             except Exception as e:
                 push_message += str(e)
     except Exception as e:
         push_message += str(e)
     finally:
-        r = requests.get('http://sc.ftqq.com/' + SCKey + '.send', params={'text': push_message, 'desp': push_message})
-        print(r.text)
+        with open(r'C:\Users\luozh\Desktop\kzz\log.txt', 'a') as f:
+            f.write(push_message+'\n')
         logger.info(push_message)
 
 
